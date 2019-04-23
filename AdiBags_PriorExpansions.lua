d4255faca2e83a29453019ab36a6371f99e1cb34
WowItsEek <49894685+WowItsEek@users.noreply.github.com>
2019-04-23 15:55:55 -0700

    Add files via upload
    First release version!

--- AdiBags_PriorExpansions.lua
+++ AdiBags_PriorExpansions.lua
@@ -13,46 +13,13 @@ local currExpansionMinID = 154177
 local currExpansionName = 'Battle for Azeroth'
 local currExpansionID = 7
 local priorMaxLevel = 110
-local kMinCloth = 152576
-local kMinHerb = 152505
-local kMinFood = 152592
-local kMinOre = 152512
-local kMinLeather = 152541
-local kMinEnchanting = 152875
-local kMinGems = 153700
-local kMinItemEnhancement = 153430
 local kMinWeapon = 124525
 local kMinArmor = 154177
-local kMinPotion = 151609
+local kCategory = 'Prior Expansion'
+local kPfx = '#'
 
-
-local kWoW = 1
-local kTBC = 2
-local kWrath = 3
-local kCata = 4
-local kMoP = 5
-local kLegion = 6
-local kBfA = 7
-
-local setKey = {'Expansion ID', 'Expansion Name', 'Tradeskill:Cloth','Tradeskill:Herb', 'Tradeskill:Food','Tradeskill:Metal & Stone','Tradeskill:Leather','Tradeskill:Enchanting','Tradeskill:Jewelcrafting'}
-local setWoW = {}
-
-
-local tooltip
-local function create()
-  local tip, leftside = CreateFrame("GameTooltip"), {}
-  for i = 1,6 do
-    local L,R = tip:CreateFontString(), tip:CreateFontString()
-    L:SetFontObject(GameFontNormal)
-    R:SetFontObject(GameFontNormal)
-    tip:AddFontStrings(L,R)
-    leftside[i] = L
-  end
-  tip.leftside = leftside
-  return tip
-end
-
--- The filter itself
+--array values are category/subcat,minitemid, and if 4th variable replace subcat
+local arrItemType = {'Tradeskill:Cloth:152576','Tradeskill:Herb:152505', 'Tradeskill:Food:152592','Tradeskill:Metal & Stone:152512','Tradeskill:Leather:152541','Tradeskill:Enchanting:152875','Tradeskill:Jewelcrafting:153700','Consumable:Potion:151609:Potions etc.','Consumable:Elixir:151609:Potions etc.', 'Consumable:Flask:151609:Potions etc.', 'Consumable:Food & Drink:151609', 'Item Enhancement:Weapon:151609:Item Enhancement'}
 
 local setFilter = addon:RegisterFilter("PriorExpansion", 47, 'ABEvent-1.0')
 setFilter.uiName = L['Prior Expansion Groups']
@@ -83,13 +50,13 @@ function setFilter:GetOptions()
   return {
     enable = {
       name = L['Enable prior expansion groups'],
-      desc = L['Check this if you want to group by prior expansion.'],
+      desc = L['Check this if you want to group by prior expansion. Prior expansion group bag labels are prefixed with #.'],
       width = 'double',
       type = 'toggle',
       order = 55,
     },
     priorExpansionGear = {
-      name = L['Equippable item sub-groupings'],
+      name = L['Equippable Item Sub-Groupings'],
       type = L['group'],
       inline = true,
       order = 65,
@@ -99,18 +66,18 @@ function setFilter:GetOptions()
           type = 'description',
           order = 10,
         },
-        enableBoE = {
-          name = L['Bind on Equip Gear'],
-          desc = L['Check to group Bind on Equip armor and weapons from prior expansions.'],
-          type = 'toggle',
-          order = 20,
-        },
-        enableBoP = {
-          name = L['Soulbound Gear'],
-          desc = L['Check to group Soulbound armor and weapons from prior expansions.'],
-          type = 'toggle',
-          order = 30,
-        },
+        -- enableBoE = {
+        --   name = L['Bind on Equip Gear'],
+        --   desc = L['Check to group Bind on Equip armor and weapons from prior expansions.'],
+        --   type = 'toggle',
+        --   order = 20,
+        -- },
+        -- enableBoP = {
+        --   name = L['Soulbound Gear'],
+        --   desc = L['Check to group Soulbound armor and weapons from prior expansions.'],
+        --   type = 'toggle',
+        --   order = 30,
+        -- },
         enableLegendaries = {
           name = L['Group Legendaries'],
           desc = L['Check to group Legendaries from prior expansions.'],
@@ -138,13 +105,13 @@ function setFilter:GetOptions()
         },
         enableMats = {
           name = L['Trade Goods'],
-          desc = L['Check to group Trade Goods from prior expansions.'],
+          desc = L['Check to group Trade Goods by category (Herbs, Leather, etc.) from prior expansions.'],
           type = 'toggle',
           order = 33,
         },
         enableConsumables = {
           name = L['Food, Drink & Potions'],
-          desc = L['Check to group Food, Drink, and Potions from prior expansions.'],
+          desc = L['Check to group Food, Drink, Potions, Elixirs and Flasks from prior expansions.'],
           type = 'toggle',
           order = 40,
         },
@@ -154,59 +121,77 @@ function setFilter:GetOptions()
 end
 
 function setFilter:Filter(slotData)
-    local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,  itemEquipLoc, itemIcon, itemSellPrice, _, _, bindType, expacID = GetItemInfo(slotData.itemId)
+  local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,  itemEquipLoc, itemIcon, itemSellPrice, _, _, bindType, expacID = GetItemInfo(slotData.itemId)
 
-    if (self.db.profile.enable) and (slotData.itemId) then
-        if (self.db.profile.enableMats) and (itemType == 'Tradeskill') then
-          if (itemSubType == 'Enchanting') and (slotData.itemId < kMinEnchanting) then
-              return 'Old Enchanting', 'Prior Expansion'
-          elseif itemSubType == 'Herb'  and (slotData.itemId < kMinHerb)then
-            return 'Old Herb', 'Prior Expansion'
-          elseif (itemSubType == 'Cloth') and (slotData.itemId < kMinCloth) then
-            return 'Old Cloth', 'Prior Expansion'
-          elseif (itemSubType == 'Leather') and (slotData.itemId < kMinLeather) then 
-            return 'Old Leather', 'Prior Expansion'
-          elseif (itemSubType == 'Metal & Stone') and (slotData.itemId < kMinOre) then
-            return 'Old Metal & Stone', 'Prior Expansion'
-          elseif (itemSubType == 'Cooking') and (slotData.itemId < kMinFood) then
-            return 'Old Cooking', 'Prior Expansion' 
-          elseif (itemSubType == 'Enchanting') and (slotData.itemId < kMinEnchanting) then
-            return 'Old Enchanting', 'Prior Expansion'
-          elseif (itemSubType == 'Jewelcrafting') and (slotData.itemId < kMinGems) then
-            return 'Old Jewelcrafting', 'Prior Expansion'
-          elseif (itemSubType == 'Item Enhancement') and (slotData.itemId < kMinItemEnhancement) then
-            return 'Old Item Enhancement', 'Prior Expansion'
-          end
-        elseif  (self.db.profile.enableConsumables)  and (itemType == 'Consumable')  then
-            if (itemSubType == 'Food & Drink') and (slotData.itemId < kMinFood)   then
-              return 'Old Food & Drink', 'Prior Expansion'
-            elseif ((itemSubType == 'Potion') or (itemSubType == 'Flask') or (itemSubType == 'Elixir')) and (slotData.itemId < kMinPotion) then
-              return 'Old Potions etc.', 'Prior Expansion'
-            end
-        elseif (itemType == 'Item Enhancement') and (slotData.itemId < kMinItemEnhancement) then
-              return 'Old Item Enhancement', 'Prior Expansion'
-        elseif  ((itemType == 'Weapon') or (itemType == 'Armor')) and (bindType) then
-            if (itemRarity == 5) and (self.db.profile.enableLegendaries) then --legendaries
-              return 'Legendary', 'Prior Expansion'
-            elseif (itemRarity == 6) and (self.db.profile.enableArtifacts)  then --Artifacts
-              return 'Artifact', 'Prior Expansion'
-            -- elseif (self.db.profile.enableBoE) or (self.db.profile.enableBoP) then 
-            --   --BOE -- check ilevel, item number, min level
-            --   return '^BoE or BoP'
-              -------------------
-              -------------------
-              -------------------
-              --- TEMPORARY
-
-            elseif (itemType == 'Weapon') and (slotData.itemId < kMinWeapon) then
-              return 'Old Weapon', 'Prior Expansion'
-            elseif (itemType == 'Armor') and (slotData.itemId < kMinArmor) then
-              return 'Old Armor', 'Prior Expansion'
-              -------------------
-              -------------------
-              -------------------
-            end
-        end
+  if (self.db.profile.enable == false) or (slotData.itemId == false) then 
+    return
+  end
+  -- load array category/subcat values
+  for x = 1, #arrItemType do
+    local currSubset = {}
+    local currItemType = arrItemType[x] .. ':'
+    local index = 1
+    for w in currItemType:gmatch('([^:]+)') do 
+      currSubset[index]  = w 
+      index = index +1
     end
+    currCategory = currSubset[1]
+    currSubCategory = currSubset[2]
+    currMinItemID = currSubset[3]
+    if (currSubset[4]) then
+      newSubCategory = currSubset[4]
+    else
+      newSubCategory = currSubCategory
+    end
+    -----FIX TO CHECK MATS OR CONSUMABLES CHECKED
+    if ((self.db.profile.enableMats) and (currCategory=='Tradeskill')) then
+      if (itemType == currCategory) and (itemSubType == currSubCategory) and (slotData.itemId < tonumber(currMinItemID)) then
+          return kPfx .. newSubCategory, kCategory
+      end
+    elseif (self.db.profile.enableConsumables) and ((currCategory=='Consumable') or (currCategory=='Item Enhancement')) then
+      if (itemType == currCategory) and (itemSubType == currSubCategory) and (slotData.itemId < tonumber(currMinItemID)) then
+          return kPfx .. newSubCategory, kCategory
+      end
+    -- elseif (self.db.profile.enableConsumables) and (currCategory=='Item Enhancement') then
+    --   if (itemType == currCategory) and (itemSubType == currSubCategory) and (slotData.itemId < tonumber(currMinItemID)) then
+    --       return kPfx .. newSubCategory, kCategory
+    --   end
+    end
+  end
+  -- end for Cat/Subcat loop from Array
+  -- start gear checks
+  if  ((itemType == 'Weapon') or (itemType == 'Armor')) then
+    if (itemRarity == 5) and (self.db.profile.enableLegendaries) then --legendaries
+      return 'Legendary', kCategory
+    elseif (itemRarity == 6) and (self.db.profile.enableArtifacts)  then --Artifacts
+      return 'Artifact', kCategory
+    elseif (itemType == 'Weapon') and (slotData.itemId < kMinWeapon) and (self.db.profile.enableBoP) then -- Old weapons
+        return kPfx .. 'Weapon', kCategory
+    elseif (itemType == 'Armor') and (slotData.itemId < kMinArmor) and (self.db.profile.enableBoP) then -- Old armor
+        return kPfx .. 'Armor', kCategory
+    end
+  end
 end
+    --     -------------------
+    --     -------------------
+    --     elseif (self.db.profile.enableBoE) or (self.db.profile.enableBoP) then 
+    -- local tooltip
+    -- local function create()
+    --   local tip, leftside = CreateFrame("GameTooltip"), {}
+    --   for i = 1,6 do
+    --     local L,R = tip:CreateFontString(), tip:CreateFontString()
+    --     L:SetFontObject(GameFontNormal)
+    --     R:SetFontObject(GameFontNormal)
+    --     tip:AddFontStrings(L,R)
+    --     leftside[i] = L
+    --   end
+    --   tip.leftside = leftside
+    --   return tip
+    -- end
+    --     -------------------
 
+
+  
+  
+  
+  
